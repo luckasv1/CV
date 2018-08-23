f87067386421c601154c5b26873bfa5c43e6990f
luckasv1 <luckasv1@Hotmail.com>
2018-08-23 13:18:20 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>CV for Luckas Vildner Borresen</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="CV for Luckas">
+    <meta name="author" content="Luckas Vildner Borresen">    
+    <link rel="icon" href="CV1_favicon.ico">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'> 
+    <!-- Global CSS -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- github calendar css -->
+    <link rel="stylesheet" href="assets/plugins/github-calendar/dist/github-calendar.css"
+/>
+    <!-- github acitivity css -->
+    <link rel="stylesheet" href="assets/plugins/github-activity/src/github-activity.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/octicons/2.0.2/octicons.min.css">
+    
+    <!-- Theme CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/style_modal.css">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+</head> 
+
+<body>
+    <!-- ******HEADER****** --> 
+    <header class="header">
+        <div class="container">
+            <div>
+                <img class="profile-content pull-right" src="assets/images/Luckas.png" style="border-radius: 50%; width: 20%; height: 20%;">            
+            </div>
+                <div class="profile-content pull-left">
+                <h1 class="name">Luckas Vildner Borresen</h1>
+                <h2 class="desc">EUX Datatekninker</h2>
+                <ul class="social list-inline">
+                    <li><a href="https://www.facebook.com/luckasvildner.borresen"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://www.linkedin.com/in/luckas-borresen-833ab6151/"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="tel:+4520965426"><i class="fa fa-phone"></i></a></li>
+                    <li><a href="https://www.snapchat.com/add/vildn3r"><i class="fa fa-snapchat-ghost"></i></a></li>               
+                    <li class="last-item"><a href="https://open.spotify.com/user/1194937478/playlist/1v8bGt2d3qyG0Q7TUPmmPL"><i class="fa fa-spotify"></i></a></li>                 
+                </ul> 
+            </div><!--//profile-->
+        </div><!--//container-->
+    </header><!--//header-->
+    
+    <div class="container sections-wrapper">
+        <div class="row">
+            <div class="primary col-md-8 col-sm-12 col-xs-12">
+                <section class="about section">
+                    <div class="section-inner">
+                        <h2 class="heading">EUX Datatekniker søger praktikplads</h2>
+                        <div class="content">
+                            <p>Hej! Mit navn er Luckas V. Borresen, jeg er 19 år og jeg kommer fra Randers. </p>
+                            <p>Jeg er i gang med uddannelsen EUX som datatekniker med speciale i programmering på Aarhus Tech.</p>
+                            <p>Det oplagte ved en EUX er, at jeg kan opnå 2 uddannelser. En gymnasial uddannelse, med fag som dansk og matematik på A niveau og stadig kunne få lov til at fordybe mig i mine interesser med computere og programmering.
+                            </p>    
+                               
+                        </div><!--//content-->
+                    </div><!--//section-inner-->                 
+                </section><!--//section-->
+                
+                <section class="experience section">
+                    <div class="section-inner">
+                        <h2 class="heading">Uddannelse</h2>
+                        <hr>
+                        <div class="content">
+                            <div class="item">
+                                <h3 class="title"><i class="fa fa-graduation-cap"></i> Folkeskole -<span class="place"><a href="#">Hadsundvejens skole</a></span> <span class="year">2005 - 2015</span></h3>
+                            </div><!--//item-->
+                            
+                            <div class="item">
+                                <h3 class="title"><i class="fa fa-graduation-cap"></i> Grundforløb 1 og 2 -<span class="place"><a href="https://tradium.dk/eux-uddannelser/eux-tech/eux-datatekniker/">Tradium Randers</a></span> <span class="year">2015 - 2016</span></h3>
+                            </div><!--//item-->
+                            
+                            <div class="item">
+                                <h3 class="title"><i class="fa fa-graduation-cap"></i> Hovedforløb 1 -<span class="place"><a href="https://aarhustech.dk/eux/uddannelser/eux-datatekniker">Aarhus Tech, Viby J</a></span> <span class="year">2016 - 2017</span></h3>
+                            </div><!--//item-->
+                            
+                            <div class="item">
+                                <h3 class="title">Skolepraktik (SKP) -<span class="place"><a href="https://aarhustech.dk/eux/uddannelser/eux-datatekniker">Aarhus Tech, Viby J</a></span> <span class="year">Januar 2017 - august 2017</span></h3>
+                            </div><!--//item-->
+                            
+                            <div class="item">
+                                <h3 class="title">Hovedforløb 2 -<span class="place"><a href="https://aarhustech.dk/eux/uddannelser/eux-datatekniker">Aarhus Tech, Viby J</a></span> <span class="year">Juni 2017 - December 2017)</span></h3>
+                            </div><!--//item-->
+                            
+                            <div class="item">
+                                <h3 class="title">Skolepraktik (SKP) -<span class="place"><a href="https://aarhustech.dk/eux/uddannelser/eux-datatekniker">Aarhus Tech, Viby J</a></span> <span class="year">Januar 2018 - august 2018</span></h3>
+                            </div><!--//item-->
+                            
+                        </div><!--//content-->  
+                    </div><!--//section-inner-->                 
+                </section><!--//section-->
+                
+                <section class="latest section">
+                   <div class="section-inner">
+                        <h2 class="heading"><center>Her kan du se mit CV</center></h2>
+                        <hr>
+                        <div class="content">    
+                                               
+                            <div class="item featured text-center">
+                                <div class="featured-image">
+                                   
+                                    <!-- The Modal -->
+
+                                    <img id="myImg" src="CV.JPG" alt="Her er et preview af mit CV" width="60%" height="60%">
+
+                                    <!-- Trigger the modal -->
+                                    <div id="myModal" class="modal">
+                                        <span class="close">&times;</span>
+                                        <img class="modal-content" id="img01">
+                                        <div id="caption"></div>
+                                    </div> 
+                 
+                                </div><!--//desc-->         
+                                <a class="btn btn-cta-secondary" href="CV.pdf" download><i class="fa fa-thumbs-o-up"></i>Hent mit CV</a>
+                                
+                            </div><!--//item-->                                   
+                        </div><!--//content-->  
+                    </div><!--//section-inner-->                 
+                </section><!--//section-->
+            </div><!--//primary-->
+            <div class="secondary col-md-4 col-sm-12 col-xs-12">
+                 <aside class="info aside section">
+                    <div class="section-inner">
+                        <h2 class="heading sr-only">Basic Information</h2>
+                        <div class="content">
+                            <ul class="list-unstyled">
+                                <li><i class="fa fa-map-marker"></i><span class="sr-only">lokation:</span> Randers, Danmark</li>
+                                <li><i class="fa fa-birthday-cake"></i><span class="sr-only">lokation:</span>26/06/1999</li>
+                                <li><i class="fa fa-envelope-o"></i><span class="sr-only">Email:</span><a href="mailto:luckasv1@hotmail.com">luckasv1@hotmail.com</a></li>
+                                <li><i class="fa fa-link"></i><span class="sr-only">Website:</span><a href="http://www.luckas.dk">http://www.luckas.dk</a></li>
+                            </ul>
+                        </div><!--//content-->  
+                    </div><!--//section-inner-->                 
+                </aside><!--//aside-->
+                
+                <aside class="skills aside section">
+                    <div class="section-inner">
+                        <h2 class="heading">Programming<i class="fa fa-code"></i></h2>
+                        <div class="content">
+                            <p class="intro">
+                                Oversigt over mine færdigheder indenfor programming</p>
+                            
+                            <div class="skillset">
+                               
+                                <div class="item">
+                                    <h3 class="level-title">PHP <span class="level-label" data-toggle="tooltip" data-placement="left" data-animation="true" title="You can use the tooltip to add more info...">niveau</span></h3>
+                                    <div class="level-bar">
+                                        <div class="level-bar-inner" data-level="48%">
+                                        </div>                                      
+                                    </div><!--//level-bar-->                                 
+                                </div><!--//item-->
+                                
+                                <div class="item">
+                                    <h3 class="level-title">HTML5 &amp; CSS3<span class="level-label">niveau</span></h3>
+                                    <div class="level-bar">
+                                        <div class="level-bar-inner" data-level="75%">
+                                        </div>                                      
+                                    </div><!--//level-bar-->                                 
+                                </div><!--//item-->
+                                
+                                <div class="item">
+                                    <h3 class="level-title">Bootstrap<span class="level-label">niveau</span></h3>
+                                    <div class="level-bar">
+                                        <div class="level-bar-inner" data-level="80%">
+                                        </div>                                      
+                                    </div><!--//level-bar-->                                 
+                                </div><!--//item-->
+                                
+                                <!--<p><a class="more-link" href="#"><i class="fa fa-external-link"></i> Mere på Coderwall?</a></p>-->
+                            </div>              
+                        </div><!--//content-->  
+                    </div><!--//section-inner-->                 
+                </aside><!--//section-->
+                
+                
+                
+                <aside class="education aside section">
+                    <div class="section-inner">
+                        <h2 class="heading">Personlige kvalikationer</h2>
+                        <div class="content">
+                            <div class="item">
+                                <table style="width:100%">
+                                    <tr>
+                                        <th><h3 class="title"><i class="fa fa-car"></i></h3></th>
+                                        <th><h3 class="title">Bil</h3></th>
+                                        <th><h3 class="title"><i class="fa fa-address-card"></i></h3></th>
+                                        <th><h3 class="title">Kørekort</h3></th>
+                                    </tr>
+                                    <tr>
+                                        <th><h3 class="title"><i class="fa fa-comment"></i></h3></th>
+                                        <th><h3 class="title">Åben</h3></th>
+                                        <th><h3 class="title"><i class="fa fa-th-list"></i></h3></th>
+                                        <th><h3 class="title">Dynamisk</h3></th>
+                                    </tr>
+                                    <tr>
+                                        <th><h3 class="title"><i class="fa fa-user"></i></h3></th>
+                                        <th><h3 class="title">Selvstændig</h3></th>
+                                        <th><h3 class="title"><i class="fa fa-address-card"></i></h3></th>
+                                        <th><h3 class="title">Serviceminded</h3></th>
+                                    </tr>
+                                    <tr>
+                                        <th><h3 class="title"><i class="fa fa-lightbulb-o"></i></h3></th>
+                                        <th><h3 class="title">Kreativ</h3></th>
+                                        <th><h3 class="title"><i class="fa fa-code-fork"></i></h3></th>
+                                        <th><h3 class="title">Fleksible</h3></th>
+                                    </tr>
+                                </table>
+                            </div><!--//item-->
+                        </div><!--//content-->
+                    </div><!--//section-inner-->
+                </aside><!--//section-->
+                            
+                <aside class="languages aside section">
+                    <div class="section-inner">
+                        <h2 class="heading">Sprog</h2>
+                        <div class="content">
+                            <ul class="list-unstyled">
+                                <li class="item">
+                                    <span class="title"><strong>Dansk:</strong></span>
+                                    <span class="level">Modersmål <br class="visible-xs"/><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> </span>
+                                </li><!--//item-->
+                                <li class="item">
+                                    <span class="title"><strong>Engelsk:</strong></span>
+                                    <span class="level">Flydende <br class="visible-sm visible-xs"/><i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i> <i class="fa fa-star"></i></span>
+                                </li><!--//item-->
+                            </ul>
+                        </div><!--//content-->
+                    </div><!--//section-inner-->
+                </aside><!--//section-->                                           
+            </div><!--//secondary-->    
+        </div><!--//row-->
+    </div><!--//masonry-->
+    
+    <!-- ******FOOTER****** --> 
+    <footer class="footer">
+        <div class="container text-center">
+            <h2>Telefon: 20965426 - Email: Luckasv1@hotmail.com</h2>
+        </div><!--//container-->
+    </footer><!--//footer-->
+ 
+    <!-- Javascript -->          
+    <script type="text/javascript" src="assets/plugins/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>    
+    <script type="text/javascript" src="assets/plugins/jquery-rss/dist/jquery.rss.min.js"></script> 
+    <!-- github calendar plugin -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/es6-promise/3.0.2/es6-promise.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fetch/0.10.1/fetch.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/github-calendar/dist/github-calendar.min.js"></script>
+    <!-- github activity plugin -->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.7.2/mustache.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/github-activity/src/github-activity.js"></script>
+    <!-- custom js -->
+    <script type="text/javascript" src="assets/js/main.js"></script>
+    <script type="text/javascript" src="assets/js/index_modal.js"></script>
+</body>
+</html>
